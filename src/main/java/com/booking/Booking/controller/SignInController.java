4fa5bb7360b9fde94797023078080414fe58a7bc
skand3r <skand3rhamouda@gmail.com>
2024-12-21 19:51:44 +0100

    added login error message

--- src/main/java/com/booking/Booking/controller/SignInController.java
+++ src/main/java/com/booking/Booking/controller/SignInController.java
@@ -5,13 +5,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SignInController {
 
     @GetMapping("/login")
-    public String signIn() {
+    public String signIn(@RequestParam(value = "error", required = false) String error, Model model) {
+
+        if (error != null) {
+            model.addAttribute("errorLogin", "Invalid Username or Password");
+        }
         return "login";
     }
 
